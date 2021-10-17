4343e83335670db2dd260cc01274f3db09066535
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-10-16 23:10:59 -0400

    made name login

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/success/<name>')
+def success(name):
+    return 'This was a success %s' % name
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
